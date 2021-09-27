a6300f5b6be9ad06f8c5d12ca6f9d855e637da1b
OG-Jons <jonas@marschall.pro>
2021-09-27 17:54:22 +0200

    Auth components

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Login</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Login",
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/AuthInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Login />
+    <SignUp />
+  </div>
+</template>
+
+<script>
+import Login from "@/components/Login/Login";
+import SignUp from "@/components/SignUp/SignUp";
+export default {
+  name: "AuthInput",
+  components: {
+    Login,
+    SignUp,
+  },
+};
+</script>
+
+<style scoped></style>
