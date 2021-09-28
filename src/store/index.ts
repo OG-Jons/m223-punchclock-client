86eb1b8ac330d637243d231e4576967c59212305
OG-Jons <jonas@marschall.pro>
2021-09-28 23:14:59 +0200

    token and user states + isLoggedIn getter

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    user: localStorage.getItem("user"),
+    token: localStorage.getItem("token"),
+  },
+  mutations: {
+    setUser(state, user) {
+      if (user) {
+        localStorage.setItem("user", user);
+      } else {
+        localStorage.removeItem("user");
+      }
+      state.user = user;
+    },
+    setToken(state, token) {
+      if (token) {
+        localStorage.setItem("token", token);
+      } else {
+        localStorage.removeItem("token");
+      }
+      state.token = token;
+    },
+  },
   actions: {},
   modules: {},
+  getters: {
+    isLoggedIn(state) {
+      return !!state.token;
+    },
+  },
 });
