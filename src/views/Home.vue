a09ca5fb82857cb01efae3eda752c5252f52dd61
OG-Jons <jonas@marschall.pro>
2021-09-28 23:14:14 +0200

    Added Auth and Home views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Dashboard />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Dashboard from "@/components/Dashboard/Dashboard.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld,
+    Dashboard,
   },
 });
 </script>
