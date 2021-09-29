71501b48275f48a830dd66c8b362ed317e9e682c
OG-Jons <jonas@marschall.pro>
2021-09-29 14:30:20 +0200

    Commit category view

---
+++ src/views/CategoryManagement.vue
@@ -0,0 +1,15 @@
+<template>
+  <div><Categories /></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Categories from "@/components/Categories/Categories.vue";
+
+export default Vue.extend({
+  name: "CategoryManagment",
+  components: {
+    Categories,
+  },
+});
+</script>
