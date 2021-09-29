b4e0c97a996c80291050c87cd3b541aee7c5d31e
OG-Jons <jonas@marschall.pro>
2021-09-29 14:28:41 +0200

    Seperated SignOut component

---
+++ src/components/SignOut/SignOut.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "SignOut",
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+  },
+  methods: {
+    async signOut() {
+      await this.$store.commit("setUser", null);
+      await this.$store.commit("setToken", null);
+      await this.$store.commit("setAdmin", null);
+      await this.$router.push("/auth");
+    },
+  },
+});
